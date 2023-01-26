87ac2400b6d4d4ad10589f6f5452e6f06bf76557
TenIvory <123652057+TenIvory@users.noreply.github.com>
2023-01-26 22:24:30 +0530

    Revert "Update pong.py"

--- pong.py
+++ pong.py
@@ -126,9 +126,6 @@ while True:
         ball.dx *= -1  
         winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
-        #Sucessfully created Pong game
-        #Sucessfully added to Github repo
-
         
 
 
